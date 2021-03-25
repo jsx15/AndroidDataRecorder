36c7a8a066da8dc9fd80846d43f97e1de1a5e4ad
sandra <schuart96@gmail.com>
2021-03-25 13:15:14 +0100

    [ADR-23]
    Methods for connecting to the database and execute a SQL Query

--- AndroidDataRecorder/Database/Database.cs
+++ AndroidDataRecorder/Database/Database.cs
@@ -1,7 +1,32 @@
-﻿namespace AndroidDataRecorder.Database
+﻿using System.Data.SQLite;
+namespace AndroidDataRecorder.Database
 {
+    
     public class Database
+
     {
+        private string dataSource = "C:/Users/sandra/Desktop/projekt/AndroidDataRecorder/identifier.sqlite";
+        /// <summary>
+        /// Create a variable for the Connection to the Database, the method needs the path to the database
+        /// </summary>
+        /// <param name="datasource"></param>
+        /// <returns> connection </returns>
+        public SQLiteConnection ConectionToDatabase(string datasource)
+        {
+            var connection = new SQLiteConnection("Data Source = " + datasource);
+            return connection;
+        }
         
+        
+        /// <summary>
+        ///  Creates a command for the database to execute an SQL query
+        /// </summary>
+        /// <returns> command </returns>
+        public SQLiteCommand SQLCommand()
+        {
+            var connection = ConectionToDatabase(dataSource);
+            var command = connection.CreateCommand();
+            return command;
+        }
     }
 }
