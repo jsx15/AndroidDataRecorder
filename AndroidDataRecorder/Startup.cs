4d6346178a02a7a1380d64f57eee4bbb78e402c1
ApuupA <57041097+ApuupA@users.noreply.github.com>
2021-03-25 12:30:56 +0100

    [ADR-48] Add Toast service

--- AndroidDataRecorder/Startup.cs
+++ AndroidDataRecorder/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Blazored.Toast;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -18,6 +19,7 @@ namespace AndroidDataRecorder
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
+            services.AddBlazoredToast();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
