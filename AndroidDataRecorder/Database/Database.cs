4112c667e08681014a3fb604e101a72ce973bf1d
sandra <schuart96@gmail.com>
2021-03-25 13:51:58 +0100

    [ARD-23]  Add Methods to the database
    - Fills the database with values
    - Print all entries of the database

--- AndroidDataRecorder/Database/Database.cs
+++ AndroidDataRecorder/Database/Database.cs
@@ -1,11 +1,15 @@
-﻿using System.Data.SQLite;
+﻿using System;
+using System.Data;
+using System.Data.SQLite;
+
 namespace AndroidDataRecorder.Database
 {
-    
+
     public class Database
 
     {
         private string dataSource = "C:/Users/sandra/Desktop/projekt/AndroidDataRecorder/identifier.sqlite";
+
         /// <summary>
         /// Create a variable for the Connection to the Database, the method needs the path to the database
         /// </summary>
@@ -14,19 +18,87 @@ namespace AndroidDataRecorder.Database
         public SQLiteConnection ConectionToDatabase(string datasource)
         {
             var connection = new SQLiteConnection("Data Source = " + datasource);
+            connection.Open();
             return connection;
         }
-        
-        
+
+
+      
+    
         /// <summary>
-        ///  Creates a command for the database to execute an SQL query
+        /// Insert the Values devicename, cpu, memory, timestamp to the table Resource.
+        /// Just for testing
         /// </summary>
-        /// <returns> command </returns>
-        public SQLiteCommand SQLCommand()
+        /// <param name="deviceName"></param>
+        /// <param name="cpu"></param>
+        /// <param name="memory"></param>
+        /// <param name="timestamp"></param>
+
+        public void InsertValuesInTableResources(string deviceName, int cpu, int memory, DateTime timestamp)
         {
+            // create connection to the database
             var connection = ConectionToDatabase(dataSource);
             var command = connection.CreateCommand();
-            return command;
+            
+            //insert Query
+            command.CommandText =
+                @"INSERT INTO Resources(DeviceName, CPU, Memory, Timestamp)
+                VALUES (@DeviceName, @CPU, @Memory, @Timestamp)";
+            
+            // Define paramters to insert new values in the table
+            SQLiteParameter p1 = new SQLiteParameter("@Devicename", DbType.String);
+            SQLiteParameter p2 = new SQLiteParameter("@CPU", DbType.Int32);
+            SQLiteParameter p3 = new SQLiteParameter("@Memory", DbType.Int32);
+            SQLiteParameter p4 = new SQLiteParameter("@Timestamp", DbType.DateTime);
+
+            // Add the paramters to the table
+            command.Parameters.Add(p1);
+            command.Parameters.Add(p2);
+            command.Parameters.Add(p3);
+            command.Parameters.Add(p4);
+            
+            // define the Values which will be insert to the table
+            for (int i = 0; i <= 10; i++)
+            {
+                p1.Value = string.Concat(deviceName, i);
+                p2.Value = i;
+                p3.Value = i;
+                p3.Value = DateTime.Today;
+                command.ExecuteNonQuery();
+            }
+            
+        }
+
+        /// <summary>
+        /// Shows all Entries of the table Resources
+        /// </summary>
+        public void showAllEntries()
+        {
+            // create connection to the database
+            var connection = ConectionToDatabase(dataSource);
+            var command = connection.CreateCommand();
+            
+            // Query to get the all values of the table
+            command.CommandText = 
+                @"SELECT * FROM Resources";
+            
+            // define a reader which get the entries 
+            SQLiteDataReader reader = command.ExecuteReader();
+            
+            // get the head of all columns and print them out
+            Console.WriteLine($"{reader.GetName(0), -3} " +
+                              $"{reader.GetName(1), -9} " +
+                              $"{reader.GetName(2), 8}" +
+                              $" {reader.GetName(3), 10}");
+            
+            // print all entries
+            while (reader.Read())
+            {
+                Console.WriteLine($@"{reader.GetInt32(0), -3}" + 
+                                  $"{reader.GetString(1), -9}" + 
+                                  $"{reader.GetInt32(2), 8}" + 
+                                  $"{reader.GetInt32(3), 10}");
+            }
         }
     }
 }
