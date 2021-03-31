c881c08bb0a2d797448d5ffe89d84afb3246993a
ApuupA <57041097+ApuupA@users.noreply.github.com>
2021-03-31 10:29:12 +0200

    [ADR-68] Add Configuration page

---
+++ AndroidDataRecorder/Misc/Marker.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR.Protocol;
+
+namespace AndroidDataRecorder.Misc
+{
+    public class Marker
+    {
+        public string _markerTime { get; set; }
+        public string _markerMessage { get; set; }
+
+        public Marker(string time, string message)
+        {
+            _markerMessage = message;
+            _markerTime = time;
+        }
+    }
+}
