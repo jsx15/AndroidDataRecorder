ded0402203edfdae409dd9757617063aa594dec2
jsx15 <>
2021-04-07 16:01:11 +0200

    [ADR-74] Add markerpage

--- AndroidDataRecorder/Misc/Marker.cs
+++ AndroidDataRecorder/Misc/Marker.cs
@@ -6,12 +6,21 @@ namespace AndroidDataRecorder.Misc
     public class Marker : Entry
     {
 
+        public int markerId { set; get; }
         public Marker(){}
         
         
 
         public Marker(string deviceName, DateTime time, string message)
         {
+            base.devicename = deviceName;
+            base.timeStamp = time;
+            base.message = message;
+        }
+        
+        public Marker(int markerId, string deviceName, DateTime time, string message)
+        {
+            this.markerId = markerId;
             base.devicename = deviceName;
             base.timeStamp = time;
             base.message = message;
