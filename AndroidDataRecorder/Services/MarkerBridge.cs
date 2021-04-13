85768ad33531eb0e6e7445966fa6a5bf0c3d92e6
jsx15 <>
2021-04-13 11:35:47 +0200

    [ADR-113] Allow filter per selected marker

---
+++ AndroidDataRecorder/Services/MarkerBridge.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using AndroidDataRecorder.Misc;
+
+namespace AndroidDataRecorder.Services
+{
+    public class MarkerBridge
+    {
+        public List<Marker> selectedMarkers = new List<Marker>();
+    }
+}
