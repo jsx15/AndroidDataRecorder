3a83d38aa508f0a1ffe8fdde7a1636beee50d8e5
jsx15 <>
2021-04-20 07:36:24 +0200

    [ADR-164] Add ticket count in navbar

--- AndroidDataRecorder/Services/LogBridge.cs
+++ AndroidDataRecorder/Services/LogBridge.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using AndroidDataRecorder.Misc;
 using AndroidDataRecorder.Pages;
 
@@ -6,6 +7,6 @@ namespace AndroidDataRecorder.Services
 {
     public class LogBridge
     {
-        public List<Filter> Filters = new List<Filter>();
+        public ObservableCollection<Filter> Filters = new ObservableCollection<Filter>();
     }
 }
