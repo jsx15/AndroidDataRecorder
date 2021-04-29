1ac51cd5242230fc4b6c18e6db5c58cba9d9af5a
Robin <robin.enderle96@gmail.com>
2021-04-29 10:19:30 +0200

    [ADR-257] added comments

--- AndroidDataRecorder/Screenrecord/Function.cs
+++ AndroidDataRecorder/Screenrecord/Function.cs
@@ -6,14 +6,30 @@ namespace AndroidDataRecorder.Screenrecord
 {
     public class Function
     {
+        /// <summary>
+        /// Check if screenrecord function is available -> Huawei does not have this function anymore
+        /// </summary>
+        /// <param name="device">Check if this device has function screenrecord function</param>
+        /// <returns>TRUE if usable, FALSE if not usable</returns>
         public static bool ScrRecAvailable(DeviceData device)
         {
+            //create receiver
             var receiver = new ConsoleOutputReceiver();
+            
+            //execute command and read input
             AdbServer.GetClient().ExecuteRemoteCommand("ls /system/bin/", device, receiver);
+            
+            //convert input to string
             var received = receiver.ToString();
-            var available = received.Contains("screenrecord");
-            Console.WriteLine("Screenrecord function available: " + available);
-            return available;
+            
+            //check if screenrecord is listed
+            var usable = received.Contains("screenrecord");
+            
+            
+            Console.WriteLine("Screenrecord function usable: " + usable);
+            
+            //return if screenrecord is usable or not
+            return usable;
         }
     }
 }
